c821692c6452f0853cc5d541b7b184f4cf52a2df
ishitasax <saxei934@newschool.edu>
2023-11-14 11:37:18 -0500

    new

---
+++ js trial copy/app.js
@@ -0,0 +1,23 @@
+fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    // onLeave: (origin, destination, direction) => {
+    //      const section = destination.item
+    //      const title = section.querySelector("h1");
+    //      const tl = new TimelineMax ({delay: 0.5});
+    //      tl.fromTo (title, 0.5, { y: "50", opacity= 0}, {y=0, opacity=1});
+
+    //      if (destination.index === 1) {
+    //         const image = document.querySelectorAll (".image");
+    //         const reccs = document.querySelector (".reccs");
+
+    //         tl.fromTo (image, 0.7, { x: "100%"}, { x: " -35% "}); 
+    //      }
+  
+    // } 
+
+    
+    
+
+});
+ 

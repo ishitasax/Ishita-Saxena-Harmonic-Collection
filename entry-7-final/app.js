99ea62d9930a2fe707c57749f9749241edd0ea68
ishitasax <saxei934@newschool.edu>
2023-12-01 15:39:04 -0500

    new

---
+++ entry-7-final/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('.section').clone().appendTo('.bg-loop');
+  $('.item').addClass('active');
+
+  var cnt = 0;
+  var bgTimer = setInterval(function(){
+      cnt++;
+      $('.bg-loop').attr('data-cnt', cnt);
+      if(cnt === 3) cnt = 0;
+  }, 3000);
+});

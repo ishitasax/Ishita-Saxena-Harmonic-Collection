d461ac2d22e9aa4466e1eb2a6b720bec484af41a
ishitasax <saxei934@newschool.edu>
2023-12-04 22:12:15 -0500

    new

--- about-new/app.js
+++ about-new/app.js
@@ -1,36 +1,36 @@
-const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+// const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const enhance = id => {
-  const element = document.getElementById(id),
-        text = element.innerText.split("");
+// const enhance = id => {
+//   const element = document.getElementById(id),
+//         text = element.innerText.split("");
   
-  element.innerText = "";
+//   element.innerText = "";
   
-  text.forEach((value, index) => {
-    const outer = document.createElement("span");
+//   text.forEach((value, index) => {
+//     const outer = document.createElement("span");
     
-    outer.className = "outer";
+//     outer.className = "outer";
     
-    const inner = document.createElement("span");
+//     const inner = document.createElement("span");
     
-    inner.className = "inner";
+//     inner.className = "inner";
     
-    inner.style.animationDelay = `${rand(-5000, 0)}ms`;
+//     inner.style.animationDelay = `${rand(-5000, 0)}ms`;
     
-    const letter = document.createElement("span");
+//     const letter = document.createElement("span");
     
-    letter.className = "letter";
+//     letter.className = "letter";
     
-    letter.innerText = value;
+//     letter.innerText = value;
     
-    letter.style.animationDelay = `${index * 1000 }ms`;
+//     letter.style.animationDelay = `${index * 1000 }ms`;
     
-    inner.appendChild(letter);    
+//     inner.appendChild(letter);    
     
-    outer.appendChild(inner);    
+//     outer.appendChild(inner);    
     
-    element.appendChild(outer);
-  });
-}
+//     element.appendChild(outer);
+//   });
+// }
 
-enhance("channel-link");
+// enhance("channel-link");
